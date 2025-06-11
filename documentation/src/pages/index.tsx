8880565850ffaa17ca1bbdf636e229c2c5be7924
Daniel Silvestre <danielsilvestre.work@gmail.com>
2025-06-12 00:37:08 +0200

    docs: fix redirect URL capitalization in home page

--- documentation/src/pages/index.tsx
+++ documentation/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Redirect } from '@docusaurus/router';
 
 export default function Home() {
-  return <Redirect to="/instantiate/docs/introduction" />;
+  return <Redirect to="/Instantiate/docs/introduction" />;
 }
