ddd981d403ad59ff4ff78a4c851c60e8c64e13a8
Daniel Silvestre <danielsilvestre.work@gmail.com>
2025-06-18 00:56:57 +0200

    feat(server): add rate limiting middleware with `express-rate-limit`
    Integrates `express-rate-limit` to restrict incoming requests to 60 per minute, enhancing server security and preventing abuse.

--- src/server.ts
+++ src/server.ts
@@ -5,17 +5,25 @@ import updateRoute from './api/update'
 import stacksApiRoute from './api/stacksApi'
 import stacksPageRoute from './pages/stacksPage'
 import { startHealthChecker } from './health/HealthChecker'
+import RateLimit from 'express-rate-limit'
 
 logger.info('[server] Server is starting...')
 
 dotenv.config()
 
 const app = express()
+
+const limiter = RateLimit({
+  windowMs: 60 * 1000, // 60 seconds
+  limit: 60 // max 60 requests per 60 seconds
+})
+
 app.disable('x-powered-by')
 app.use(express.json())
 app.use('/api', updateRoute)
 app.use('/api', stacksApiRoute)
 app.use('/', stacksPageRoute)
+app.use(limiter);
 
 app.get('/', (req, res) => {
   res.send('Instantiate backend is running!')
