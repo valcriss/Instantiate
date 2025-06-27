f16bd489a19e35b7e3971f6fd566e7498459c6ae
Daniel Silvestre <danielsilvestre.work@gmail.com>
2025-06-27 09:46:27 +0200

    Update MR comments on deployment errors

--- src/comments/MergeRequestCommenter.ts
+++ src/comments/MergeRequestCommenter.ts
@@ -1,6 +1,6 @@
 import { MergeRequestPayload } from '../types/MergeRequestPayload'
 
-export type DeploymentStatus = 'in_progress' | 'ready' | 'closed'
+export type DeploymentStatus = 'in_progress' | 'ready' | 'closed' | 'error'
 
 export interface MergeRequestCommenter {
   postStatusComment(payload: MergeRequestPayload, status: DeploymentStatus, links?: Record<string, string>): Promise<void>
