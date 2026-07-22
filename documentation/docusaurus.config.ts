0007cc20b8ac2d2664b45bcfaa65dfe04da4ad9a
Daniel Silvestre <danielsilvestre.work@gmail.com>
2026-07-22 02:13:07 +0200

    chore: update Docusaurus dependencies to version 3.10.2 and webpack-dev-server to version 6.0.0; add overrides for various packages

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -26,7 +26,11 @@ const config: Config = {
   projectName: 'instantiate', // Usually your repo name.
   deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
